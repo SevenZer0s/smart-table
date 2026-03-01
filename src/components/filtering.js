4cb74d6d66e26b4d40e71c16590df0601d559dfe
Klochkov Makar Vadimovich <Klochkov@example.com>
2026-03-01 18:37:04 +0300

    Данные получаются с сервера, сделан рефакторинг кода

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,33 +1,39 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
-
-// @todo: #4.3 — настроить компаратор
-const compare = createComparison(defaultRules)
-
-export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
-        Object.keys(indexes)   // Получаем ключи из объекта
-            .forEach((elementName) => {  // Перебираем по именам
-                elements[elementName].append(  // в каждый элемент добавляем опции
-                    ...Object.values(indexes[elementName]) // формируем массив имён, значений опций
-                            .map(name => { 
-                                const option = document.createElement('option')
-                                option.setAttribute('value', name)
-                                option.textContent = name
-                                return option
-                            })
-                )
+export function initFiltering(elements) {
+    const updateIndexes = (elements, indexes) => {
+        Object.keys(indexes).forEach(elementName => {
+        elements[elementName].append(
+            ...Object.values(indexes[elementName]).map(name => {
+                const option = document.createElement('option');
+                option.value = name;
+                option.textContent = name;
+                return option;
             })
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+        )
+    })
+    }
+    
+    const applyFiltering = (query, state, action) => {
         if (action && action.name === 'clear') {
-            const button = action;
-            const parent = button.parentElement
-            const input = parent.querySelector('input');
-            input.value = ""
-            state[input.name] = ""
+            const parent = action.parentElement;
+            const input = parent.querySelector('input')
+            input.value = '';
+            state[action.dataset.field] = '';
         }
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data.filter(row => compare(row, state));
-        return data;
+
+        const filter = {};
+        Object.keys(elements).forEach(key => {
+            if (elements[key]) {
+                if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) { 
+                    filter[`filter[${elements[key].name}]`] = elements[key].value; 
+                }
+            }
+        })
+        return Object.keys(filter).length ? Object.assign({}, query, filter) : query;
+    }
+    
+
+    return {
+        updateIndexes,
+        applyFiltering,
     }
 }
